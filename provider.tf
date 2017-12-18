7be347315acddfbeb4101c73dd4acef29d3998c0
jeremie <jeremie@millenard.com>
2017-12-18 14:08:39 +0100

    readme

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "scaleway" {
-  organization = "<my_id>"
-  token        = "<my_secret>"
-  region       = "par1" # could be Amsterdam too
+  organization = "a233b8ab-49ed-4209-808d-0ad7c80c484c"
+  token        = "ae4b034c-ab52-4594-a3a4-8b7735c3eb34"
+  region       = "par1"
 }
